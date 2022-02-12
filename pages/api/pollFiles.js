00703734590cdd78745816875df1c516a844d8d3
Christy Campbell <christytc10@gmail.com>
2022-02-12 15:43:51 +0000

    Update ftp pulling

--- pages/api/pollFiles.js
+++ pages/api/pollFiles.js
@@ -1,16 +1,26 @@
-const jsftp = require("jsftp");
+const ftp = require("basic-ftp")
 
-export default function handler(req, res) {
-  const ftp = new jsftp({
-    host: `${process.env.RACE_DATA_FTP_ADDRESS}`,
-    port: process.env.RACE_DATA_FTP_PORT,
-    user: `${process.env.RACE_DATA_FTP_USER}`,
-    pass: `${process.env.RACE_DATA_FTP_PASS}`,
-  });
+export default async function handler(req, res) {
+  const client = new ftp.Client()
 
-  ftp.list(process.env.RACE_DATA_FTP_REMOTE_DIR, (err, res) => {
-    if (err) console.log(res, err);
-  });
+  try {
+    await client.access({
+        secure: true,
+        host: `${process.env.RACE_DATA_FTP_ADDRESS}`,
+        port: process.env.RACE_DATA_FTP_PORT,
+        user: `${process.env.RACE_DATA_FTP_USER}`,
+        password: `${process.env.RACE_DATA_FTP_PASS}`,
+        secureOptions: {rejectUnauthorized:false}
+    })
+    const files = await client.list(process.env.RACE_DATA_FTP_REMOTE_DIR);
+    files.forEach(f => {
+      console.log(f.name)
+    })
+  } catch(err) {
+      console.log(err)
+  } finally {
+    client.close();
+  }
   //TODO - add files wanted to DB or queue
 
   res.status(200).json({ status: "OK" });
