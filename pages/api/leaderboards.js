a454ffe8b85e4d5e109085b47c1deedd87f60348
Christy Campbell <christytc10@gmail.com>
2022-04-04 08:48:31 +0100

    Leaderboard TODOs

--- pages/api/leaderboards.js
+++ pages/api/leaderboards.js
@@ -9,13 +9,18 @@ export default async function handler(req, res) {
   }
   const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, supabaseOptions)
 
-  const { data: results_files, error } = await supabase
+  const { data: leaderboard, error } = await supabase
     .from('time_trial_laps')
     .select("*")
-    .eq('session_track', 'hungaroring')
+    .eq('session_track', 'zandvoort')
     .order('bestLap', { ascending: true })
 
-  console.log(results_files)
+    //TODO - check the pruning code re: car class
+    //TODO - track as path param
+    //TODO - render results filtered by class
+    //TODO - filter in code here by driver ID as well to double check dupes
 
-  res.status(200).json({ results_files });
+  console.log(leaderboard)
+
+  res.status(200).json({ leaderboard });
 }
