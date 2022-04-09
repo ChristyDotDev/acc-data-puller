f7bed92f3c68072ef5c3c4112ee76464d4914c47
Christy Campbell <christytc10@gmail.com>
2022-04-09 08:29:23 +0100

    Leaderboard route

--- pages/api/leaderboard/[track].js
+++ pages/api/leaderboard/[track].js
@@ -1,6 +1,3 @@
-const ftp = require("basic-ftp")
-const stream = require('stream');
-import { readFile, readFileSync } from 'fs';
 import { createClient } from '@supabase/supabase-js'
 
 export default async function handler(req, res) {
@@ -12,15 +9,12 @@ export default async function handler(req, res) {
   const { data: leaderboard, error } = await supabase
     .from('time_trial_laps')
     .select("*")
-    .eq('session_track', 'zandvoort')
+    .eq('session_track', req.query.track)
     .order('bestLap', { ascending: true })
 
-    //TODO - check the pruning code re: car class
-    //TODO - track as path param
-    //TODO - render results filtered by class
-    //TODO - filter in code here by driver ID as well to double check dupes
-
-  console.log(leaderboard)
-
+  if (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
   res.status(200).json({ leaderboard });
 }
