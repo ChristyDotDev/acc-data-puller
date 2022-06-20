3084d46c529ddb484dd9ec540739124ed306301b
Christy Campbell <christytc10@gmail.com>
2022-06-20 22:29:45 +0100

    elo ranking outline

---
+++ pages/api/elocalc.js
@@ -0,0 +1,11 @@
+import EloRating from 'elo-rating';
+
+export default async function handler(req, res) {
+    console.log(EloRating.calculate(2100, 1400));
+    //TODO - when new file detected and type is race, stick it in the elo table
+    //TODO - Parse the file and get the results ordering
+    //TODO - grab every racer's current ELO (with default/starting ELO if none found)
+    //TODO - calculate new ELO per racer
+    //TODO - persist new ELOs
+    res.status(200).send();
+}
